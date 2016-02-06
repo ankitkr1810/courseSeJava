b2ce6c441f0efca03cde52ccc58c5904eaacd495
gitHB <githb@yandex.ru>
2016-02-07 00:45:13 +0500

    first test

--- Project/src/test/java/hb/tests/TestBase.java
+++ Project/src/test/java/hb/tests/TestBase.java
@@ -1,9 +1,10 @@
 package hb.tests;
 
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import org.testng.annotations.AfterSuite;
@@ -25,22 +26,27 @@ public class TestBase {
 
 	@BeforeClass
 	public void init() {
+
 		baseUrl = PropertyLoader.loadProperty("site.url");
 		gridHubUrl = PropertyLoader.loadProperty("grid2.hub");
-
+		FirefoxProfile firefoxProfile = new FirefoxProfile();
+		firefoxProfile.setPreference("xpinstall.signatures.required", false);
 		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(FirefoxDriver.PROFILE, firefoxProfile);
 		capabilities.setBrowserName(PropertyLoader.loadProperty("browser.name"));
 		capabilities.setVersion(PropertyLoader.loadProperty("browser.version"));
 		String platform = PropertyLoader.loadProperty("browser.platform");
 		if (!(null == platform || "".equals(platform))) {
 			capabilities.setPlatform(Platform.valueOf(PropertyLoader.loadProperty("browser.platform")));
 		}
-
+		
+		//driver = new FirefoxDriver(firefoxProfile);
 		if (!(null == gridHubUrl || "".equals(gridHubUrl))) {
 			driver = WebDriverFactory.getDriver(gridHubUrl, capabilities);
 		} else {
 			driver = WebDriverFactory.getDriver(capabilities);
 		}
+		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
