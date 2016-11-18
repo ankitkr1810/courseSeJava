8292f938a4fe9bc4f7a84a55bd1aa1fe90afa370
gitHB <githb@yandex.ru>
2016-11-19 00:18:32 +0500

    login test

--- src/test/java/ru/sd/tests/seljava/FirstTest.java
+++ src/test/java/ru/sd/tests/seljava/FirstTest.java
@@ -9,8 +9,7 @@
         import org.openqa.selenium.firefox.FirefoxDriver;
         import org.openqa.selenium.remote.DesiredCapabilities;
         import org.openqa.selenium.support.ui.WebDriverWait;
-        import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
-
+        import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 public class FirstTest {
 
     public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
@@ -37,10 +36,11 @@ public class FirstTest {
 
     @Test
     public void myFirstTest() {
-        driver.navigate().to("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(visibilityOfElementLocated(By.id("sidebar")));
     }
     @After
     public void stop() {
